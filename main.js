041cd7c6b2958127b092de2bf896471282d91353
Antonio <antony.dovzhenko@gmail.com>
2024-02-27 17:19:19 +0200

    added converting function this function is converting md files into html files by adding html tags

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ fs.readFile(inputFile, 'utf8', (err, data) => {
         process.exit(1);
     }
 
+    const html = convertMarkdownToHtml(data);
+
     if (outputFile) {
         fs.writeFile(outputFile, html, (err) => {
             if (err) {
@@ -26,3 +28,39 @@ fs.readFile(inputFile, 'utf8', (err, data) => {
         console.log(html);
     }
 });
+
+function convertMarkdownToHtml(markdown) {
+    let html = ' ';
+    const lines = markdown.split('\n');
+    let inPreBlock = false;
+    let newParagraph = true;
+
+    lines.forEach((line, index) => {
+        if (line.startsWith('```')) {
+            inPreBlock = !inPreBlock;
+            html += inPreBlock ? '<pre>' : '</pre>\n';
+            newParagraph = true;
+        } else if (inPreBlock) {
+            html += `${line}\n`;
+        } else {
+            if (line.trim() !== '') {
+                if (newParagraph && html !== '') {
+                    html += '<p>';
+                }
+                newParagraph = false;
+
+                line = line.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>')
+                    .replace(/__(.*?)__/g, '<b>$1</b>')
+                    .replace(/_(.*?)_/g, '<i>$1</i>')
+                    .replace(/`(.*?)`/g, '<code>$1</code>');
+
+                html += line;
+                if ((lines[index + 1] && lines[index + 1].trim() === '') || index === lines.length - 1) {
+                    html += '</p>\n';
+                    newParagraph = true;
+                }
+            }
+        }
+    });
+    return html;
+}
