621acdb8f00cb3887d2f838b47260fc219c338b9
Antonio <antony.dovzhenko@gmail.com>
2024-02-27 17:23:02 +0200

    added unmatched markers check function checks that all markers are closed (matched)

--- main.js
+++ main.js
@@ -14,6 +14,11 @@ fs.readFile(inputFile, 'utf8', (err, data) => {
         process.exit(1);
     }
 
+    if (!checkForUnmatchedMarkers(data)) {
+        console.error('Error: Unmatched markdown markers found');
+        process.exit(1);
+    }
+
     const html = convertMarkdownToHtml(data);
 
     if (outputFile) {
@@ -63,4 +68,47 @@ function convertMarkdownToHtml(markdown) {
         }
     });
     return html;
+}
+
+function checkForUnmatchedMarkers(markdown) {
+    let isBoldOpen = false;
+    let isMonospaceOpen = false;
+    let isItalicOpen = false;
+    let preformattedCount = 0;
+
+    for (let i = 0; i < markdown.length; i++) {
+
+        if (markdown[i] === '*' && i + 1 < markdown.length && markdown[i + 1] === '*') {
+            if (!(i > 0 && markdown[i - 1] === '\\')) {
+                isBoldOpen = !isBoldOpen;
+                i++;
+            }
+        }
+
+        if (markdown[i] === '`' && (i < 1 || markdown[i - 1] !== '`')) {
+            if (!(i > 0 && markdown[i - 1] === '\\')) {
+                isMonospaceOpen = !isMonospaceOpen;
+            }
+        }
+
+        if (markdown[i] === '_') {
+            if (!(i > 0 && markdown[i - 1] === '\\')) {
+                let isPartOfWord = (i > 0 && markdown[i - 1].match(/\w/)) && (i < markdown.length - 1 && markdown[i + 1].match(/\w/));
+                if (!isPartOfWord) {
+                    isItalicOpen = !isItalicOpen;
+                }
+            }
+        }
+
+        if (markdown[i] === '`' && i + 2 < markdown.length && markdown[i + 1] === '`' && markdown[i + 2] === '`') {
+            preformattedCount++;
+            i += 2;
+        }
+    }
+
+    if (isBoldOpen || isMonospaceOpen || isItalicOpen || preformattedCount % 2 !== 0) {
+        return false;
+    }
+
+    return true;
 }
